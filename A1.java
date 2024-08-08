9ce61a749901a44f44dd885a5621251f9150d5ad
Manish Mishra <manish.se1094@gmail.com>
2024-08-08 17:49:38 +0530

    m2 method executed

--- A1.java
+++ A1.java
@@ -2,9 +2,17 @@ Class A{
     public void m1(){
         //code
     }
+<<<<<<< Updated upstream
 
     public void m3(){
         // code1
         // code2
     }
+=======
+public m2(){
+    // code2
+    // code3
+}
+    
+>>>>>>> Stashed changes
 }
